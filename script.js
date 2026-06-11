bd7bc003be1cd2f8f1ad9572cf960ea87ff9245e
Niels <niels.saura@gmail.com>
2026-06-11 21:25:01 +0200

    ajout du beep

--- script.js
+++ script.js
@@ -1,15 +1,58 @@
 //  temps en secondes, la seconde étant l'unité de rafraichissement
-const progressTime=20*60;
-const pauseTime=1.5*60;
+const progressTime=5;
+const pauseTime=3;
 
 const circle=document.getElementsByTagName("circle")[0];
 const timeDisplay=document.getElementsByClassName("time-display")[0];
 const docTitle=document.querySelector("head title");
+const bouttonSon=document.getElementById("son");
+
+let audioContext=false;
+
+function handleSound() {
+    isOn=bouttonSon.classList.toggle("on");
+    if (isOn) {
+        audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        bouttonSon.innerHTML="&#128266;";
+    } else {
+        audioContext = false;
+        bouttonSon.innerHTML="&#128263;";
+    };
+}
+
+bouttonSon.addEventListener("click", handleSound);
 
 const perimeter=2*Math.PI*80;
 
 let targetTime=progressTime;
 let progress=0;
+
+let continuousBeep;
+
+function playBeep() {
+
+    if (audioContext){
+
+        const oscillator = audioContext.createOscillator();
+        const gainNode = audioContext.createGain();
+
+        oscillator.connect(gainNode);
+        gainNode.connect(audioContext.destination);
+
+        oscillator.type = "sine";      // sine, square, sawtooth, triangle
+        oscillator.frequency.value = 1000; // fréquence en Hz
+
+        oscillator.start();
+
+        gainNode.gain.setValueAtTime(0.2, audioContext.currentTime);
+        gainNode.gain.exponentialRampToValueAtTime(
+            0.0001,
+            audioContext.currentTime + 0.2);
+    
+
+        oscillator.stop(audioContext.currentTime + 0.2);
+        };
+}
 
 function reset() {
     progress=0;
@@ -21,6 +64,7 @@ function changePhase() {
     progressInterval=setInterval(progressStep, 1000); // mise à jour toutes les secondes
     timeDisplay.removeEventListener("click", changePhase);
     timeDisplay.addEventListener("click", reset);
+    clearInterval(continuousBeep);
 };
 
 function progressStep() {
@@ -46,6 +90,7 @@ function progressStep() {
         circle.style.stroke="green";
         timeDisplay.removeEventListener("click", reset);
         timeDisplay.addEventListener("click", changePhase);
+        continuousBeep=setInterval(playBeep, 500);
     };
 };
 
